d5c40a7b6d1d249c65fc240005c3680c4395dc3a
Fernando Rodrigo Aguilar Dorado <altekfer@gmail.com>
2023-09-12 22:32:07 -0300

    Actualización de carpetas de clase

---
+++ Clase11/js/app.js
@@ -0,0 +1,42 @@
+// comentario de línea
+
+/* 
+Comentario multilinea
+alert("Prueba Script")
+
+confirm("¿Estás seguro?"); // ventana de confirmación
+location = "https://google.com" // abrir una página diferente luego de la confirmación
+prompt("¿Qué edad tienes?", "20"); // muestra un mensaje que puede alojar un dato
+
+console.error("Este es un error")
+let i = 10;
+console.log(i);
+
+const SOY_UNA_CONSTANTE = "Soy una constante";
+console.log(SOY_UNA_CONSTANTE);
+
+let a = 10;
+let b;
+let c = "Hola";
+let d = true;
+let e = null;
+let f = 4-1;
+let g = Number("2"); //casteo de tipo de dato
+
+console.log("El tipo de dato a es:", typeof(a)); // se puede "ver" el tipo de dato de la variable
+console.log("El tipo de dato b es:", typeof (b));
+console.log("El tipo de dato c es:", typeof (c));
+console.log("El tipo de dato d es:", typeof (d));
+console.log("El tipo de dato e es:", e);
+console.log("El contenido de f es:", f);
+console.log("El tipo de dato f es:", typeof (f));
+console.log("El contenido de g es:", g);
+console.log("El tipo de dato g es:", typeof (g));
+
+*/
+
+let a = 4;
+let b = 3;
+let c = a % b;
+
+console.log(c);
